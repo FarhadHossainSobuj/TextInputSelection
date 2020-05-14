0e6a2c4741b6f07e66077ad61f6f17cc9be413f2
Farhad Hossain <tlcfarhad@gmail.com>
2020-05-14 13:16:55 +0600

    initial commit

--- app/src/main/java/com/example/textinputselection/MainActivity.java
+++ app/src/main/java/com/example/textinputselection/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.textinputselection;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.tomer.fadingtextview.FadingTextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FadingTextView fadingTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fadingTextView = findViewById(R.id.fading_text_view);
+
+    }
+
+    public void startExample2(View v){
+        String[] example2 = {"And", "this", "is", "example 2"};
+        fadingTextView.setTexts(example2);
+        fadingTextView.setTimeout(300, FadingTextView.MILLISECONDS);
     }
 }
