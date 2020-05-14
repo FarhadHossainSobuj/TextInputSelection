9e14f5b11d6dc23441317e02b8a35b4220cdea0a
Farhad Hossain <tlcfarhad@gmail.com>
2020-05-14 13:42:59 +0600

    initial commit

--- app/src/main/java/com/example/textinputselection/MainActivity.java
+++ app/src/main/java/com/example/textinputselection/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.textinputselection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 public class MainActivity extends AppCompatActivity {
+    private TextSwitcher textSwitcher;
+    private Button nextButton;
+    private int stringIndex = 0;
+    private String[] row = {"One", "Two", "Three", "Four", "Five", "Six"};
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textSwitcher = findViewById(R.id.textSwither);
+        nextButton = findViewById(R.id.button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(stringIndex == row.length-1){
+                    stringIndex = 0;
+                    textSwitcher.setText(row[stringIndex]);
+                } else {
+                    textSwitcher.setText(row[++stringIndex]);
+                }
+            }
+        });
+        textSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                textView = new TextView(MainActivity.this);
+                textView.setTextColor(Color.BLACK);
+                textView.setTextSize(60);
+                textView.setGravity(Gravity.CENTER_HORIZONTAL);
+                return textView;
+            }
+        });
+        textSwitcher.setText(row[stringIndex]);
     }
 }
